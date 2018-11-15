80e367e360d0f01ec2c62d4f51783b79dbc460c8
evrenke <evrenkeskin300@gmail.com>
2018-11-14 17:06:01 -0800

    Update Contact.h

--- Contact.h
+++ Contact.h
@@ -4,9 +4,11 @@ class Contact {
 	private:
 		//Location location;
 		std::string name;
+		std::string address;
 		int contactNum;
 	public:
 		std::string getName() { return name; }
+		std::string getAdress() { return address;}
 		int getContactNum() { return contactNum; }
 		//location getLocation(){ return location; }
 };
